5ee733ba5128d789b227dcec7080c6283a820d60
Mike Allen <mwjallen@live.co.uk>
2021-12-23 14:11:34 +0000

    Changes to design

---
+++ Models/TableBooking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BeanAndBrew.Models
+{
+    public class TableBooking
+    {
+        [Key]
+        public int BookingId { get; set; }
+        public string Restaurant { get; set; }
+        public int NoOfTables { get; set; }
+        public DateTime BookingDate { get; set; }
+    }
+}
